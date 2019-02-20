6c3c8a79d895441356aed97be097a037e1441bb3
NaiveD <30673867+NaiveD@users.noreply.github.com>
2019-02-20 17:59:05 +0800

    q6

---
+++ q6.py
@@ -0,0 +1,76 @@
+"""
+Finding the max value of f(x) = xsin(10πx) + 2, -1 <= x <= 2 and
+visualize the Genetic Algorithm.
+
+"""
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+DNA_SIZE = 10            # DNA length
+POP_SIZE = 100           # population size
+CROSS_RATE = 0.8         # mating probability (DNA crossover)
+MUTATION_RATE = 0.003    # mutation probability
+N_GENERATIONS = 200
+X_BOUND = [-1, 2]         # x upper and lower bounds
+
+# to find the maximum of this function
+def F(x): 
+	return x * np.sin(10 * math.pi * x) + 2    
+
+# find non-zero fitness for selection
+def get_fitness(pred): 
+	return pred + 1e-3 - np.min(pred) # minus the smallest value to make fitness positive, add a small number 1e-3 to ensure that fitness is not zero
+
+
+# convert binary DNA to decimal and normalize it to a range(-1, 2)
+def translateDNA(pop): return pop.dot(2 ** np.arange(DNA_SIZE)[::-1]) / float(2**DNA_SIZE-1) * X_BOUND[1]
+
+
+def select(pop, fitness):    # nature selection with regard to pop's fitness
+    idx = np.random.choice(np.arange(POP_SIZE), size=POP_SIZE, replace=True,
+                           p=fitness/fitness.sum())
+    return pop[idx]
+
+
+def crossover(parent, pop):     # mating process (genes crossover)
+    if np.random.rand() < CROSS_RATE:
+        i_ = np.random.randint(0, POP_SIZE, size=1)                             # select another individual from pop
+        cross_points = np.random.randint(0, 2, size=DNA_SIZE).astype(np.bool)   # choose crossover points
+        parent[cross_points] = pop[i_, cross_points]                            # mating and produce one child
+    return parent
+
+
+def mutate(child):
+    for point in range(DNA_SIZE):
+        if np.random.rand() < MUTATION_RATE:
+            child[point] = 1 if child[point] == 0 else 0
+    return child
+
+
+pop = np.random.randint(2, size=(POP_SIZE, DNA_SIZE))   # initialize the pop DNA
+
+# something about plotting
+plt.ion()       
+x = np.linspace(*X_BOUND, 200)
+plt.plot(x, F(x))
+
+for _ in range(N_GENERATIONS):
+    F_values = F(translateDNA(pop))    # compute function value by extracting DNA
+
+    # something about plotting
+    if 'sca' in globals(): sca.remove()
+    sca = plt.scatter(translateDNA(pop), F_values, s=200, lw=0, c='red', alpha=0.5); plt.pause(0.05)
+
+    # GA part (evolution)
+    fitness = get_fitness(F_values)
+    print("Generation ", _ , ". Most fitted DNA: ", pop[np.argmax(fitness), :], ", Function value = ", F_values[np.argmax(fitness)], ", x = ", translateDNA(pop)[np.argmax(fitness)])
+    # perform reproduction (have some problems)
+    pop = select(pop, fitness)
+    pop_copy = pop.copy()
+    for parent in pop:
+        child = crossover(parent, pop_copy)
+        child = mutate(child)
+        parent[:] = child       # parent is replaced by its child
+
+plt.ioff(); plt.show()
